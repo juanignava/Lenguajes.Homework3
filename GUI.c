79f9977564d5b99cffa8051c363c6d19cd7df67a
Juan Ignacio Navarro <ju.navarro@estudiantec.cr>
2021-04-19 00:15:31 -0600

    GUI update

--- GUI.c
+++ GUI.c
@@ -1,5 +1,23 @@
 #include <gtk/gtk.h>
 
+// Constants
+const int WINDOW_HEIGHT = 1000;
+const int WINDOW_WIDTH = 650;
+const int WIDTH_MARGIN = 50;
+const int HEIGHT_MARGIN = 25;
+const int BASE_HEIGHT_1 = 605;
+const int BASE_HEIGHT_2 = 335;
+const int BASE_HEIGHT_3 = 200;
+const int BASE_WIDTH = 164;
+const int ROPE_WIDTH = 20;
+const int ROPE_HEIGHT = 35;
+const struct DK_POSITION
+{
+    int x_pos;
+    int y_pos;
+} DK_POSITION = {50,565};
+
+
 GtkWidget* window, *layout, *deedee_kong;
 
 int main(int argc, char* argv[])
@@ -14,39 +32,39 @@ int main(int argc, char* argv[])
     gtk_window_set_resizable(GTK_WINDOW(window), FALSE);
     gtk_widget_add_events(window, GDK_KEY_PRESS_MASK);
     g_signal_connect(window, "delete-event", G_CALLBACK(gtk_main_quit), NULL);
-    gtk_widget_set_size_request(window, 1000, 650);
+    gtk_widget_set_size_request(window, WINDOW_HEIGHT, WINDOW_WIDTH);
 
     layout = gtk_fixed_new();
 
     base_1 = gtk_image_new_from_file("images/base.png");
-    gtk_fixed_put(GTK_FIXED(layout), base_1, 50, 605);
+    gtk_fixed_put(GTK_FIXED(layout), base_1, WIDTH_MARGIN, BASE_HEIGHT_1);
     base_2 = gtk_image_new_from_file("images/base.png");
-    gtk_fixed_put(GTK_FIXED(layout), base_2, 234, 335);
+    gtk_fixed_put(GTK_FIXED(layout), base_2, WIDTH_MARGIN + BASE_WIDTH + ROPE_WIDTH, BASE_HEIGHT_2);
     base_3 = gtk_image_new_from_file("images/base.png");
-    gtk_fixed_put(GTK_FIXED(layout), base_3, 418, 605);
+    gtk_fixed_put(GTK_FIXED(layout), base_3, WIDTH_MARGIN + 2*BASE_WIDTH + 2*ROPE_WIDTH, BASE_HEIGHT_1);
     base_4 = gtk_image_new_from_file("images/base.png");
-    gtk_fixed_put(GTK_FIXED(layout), base_4, 602, 335);
+    gtk_fixed_put(GTK_FIXED(layout), base_4, WIDTH_MARGIN + 3*BASE_WIDTH + 3*ROPE_WIDTH, BASE_HEIGHT_2);
     base_5 = gtk_image_new_from_file("images/base.png");
-    gtk_fixed_put(GTK_FIXED(layout), base_5, 786, 605);
+    gtk_fixed_put(GTK_FIXED(layout), base_5, WIDTH_MARGIN + 4*BASE_WIDTH + 4*ROPE_WIDTH, BASE_HEIGHT_1);
     base_6 = gtk_image_new_from_file("images/base.png");
-    gtk_fixed_put(GTK_FIXED(layout), base_6, 786, 200);
+    gtk_fixed_put(GTK_FIXED(layout), base_6, WIDTH_MARGIN + 4*BASE_WIDTH + 4*ROPE_WIDTH, BASE_HEIGHT_3);
 
     rope_1 = gtk_image_new_from_file("images/rope.png");
-    gtk_fixed_put(GTK_FIXED(layout), rope_1, 214, 35);
+    gtk_fixed_put(GTK_FIXED(layout), rope_1, WIDTH_MARGIN + BASE_WIDTH, ROPE_HEIGHT);
     rope_2 = gtk_image_new_from_file("images/rope.png");
-    gtk_fixed_put(GTK_FIXED(layout), rope_2, 398, 35);
+    gtk_fixed_put(GTK_FIXED(layout), rope_2, WIDTH_MARGIN + 2*BASE_WIDTH + ROPE_WIDTH, ROPE_HEIGHT);
     rope_3 = gtk_image_new_from_file("images/rope.png");
-    gtk_fixed_put(GTK_FIXED(layout), rope_3, 582, 35);
+    gtk_fixed_put(GTK_FIXED(layout), rope_3, WIDTH_MARGIN + 3*BASE_WIDTH + 2*ROPE_WIDTH, ROPE_HEIGHT);
     rope_4 = gtk_image_new_from_file("images/rope.png");
-    gtk_fixed_put(GTK_FIXED(layout), rope_4, 766, 35);
+    gtk_fixed_put(GTK_FIXED(layout), rope_4, WIDTH_MARGIN + 4*BASE_WIDTH + 3*ROPE_WIDTH, ROPE_HEIGHT);
 
     donkey_kong = gtk_image_new_from_file("images/donkey_kong.png");
     gtk_fixed_put(GTK_FIXED(layout), donkey_kong, 850, 120);
     deedee_kong = gtk_image_new_from_file("images/deedee_kong.png");
-    gtk_fixed_put(GTK_FIXED(layout), deedee_kong, 50, 565);
+    gtk_fixed_put(GTK_FIXED(layout), deedee_kong, DK_POSITION.x_pos, DK_POSITION.y_pos);
 
     long_base = gtk_image_new_from_file("images/long_base.png");
-    gtk_fixed_put(GTK_FIXED(layout), long_base, 50, 25);
+    gtk_fixed_put(GTK_FIXED(layout), long_base, WIDTH_MARGIN, HEIGHT_MARGIN);
 
     gtk_container_add(GTK_CONTAINER(window), layout);
     
